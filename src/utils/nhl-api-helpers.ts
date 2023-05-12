f3388e8df435cb112e2eda67c4b4340d8781fb76
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-05-12 14:27:29 -0400

    Added getAllNHLYears()

--- src/utils/nhl-api-helpers.ts
+++ src/utils/nhl-api-helpers.ts
@@ -1,5 +1,32 @@
-function getAllNHLYears(json: JSON): number[] {
-    return [];
+/**
+ * Returns an array of all the years the player has active stats in the NHL.
+ * The array contains pairs of years [startYear, endYear].
+ * 
+ * @param json The JSON string form the NHL API
+ * @returns an array of all the years the player has active stats in the NHL
+ */
+function getAllNHLYears(json: string): number[][] {
+    const years: number[][] = [];
+    const data = JSON.parse(json).stats[0].splits;
+
+    for (const i in data) {
+        const league: string = data[i].league.name;
+
+        if (league == "National Hockey League") {
+            const yearString: string = data[i].season;
+            const yearPair: number[] = [];
+
+            const yearStart: number = parseInt(yearString.substring(0, 4));
+            const yearEnd: number = parseInt(yearString.substring(4));
+
+            yearPair.push(yearStart);
+            yearPair.push(yearEnd);
+
+            years.push(yearPair);
+        }
+    }
+
+    return years;
 }
 
 function getNHLGoalsPerYear(json: JSON): number[] {
@@ -14,5 +41,8 @@ const requestOptions: RequestInit = {
   
 fetch("https://statsapi.web.nhl.com/api/v1/people/8476981/stats?stats=yearByYear", requestOptions)
     .then(response => response.text())
-    .then(result => console.log(JSON.parse(result)))
+    .then((result) => { 
+        console.log(getAllNHLYears(result));
+        console.log(JSON.parse(result).stats[0].splits) 
+    })
     .catch(error => console.log('error', error));
