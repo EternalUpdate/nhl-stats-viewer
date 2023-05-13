27df8bbd9bac1ef2c9f2a403fdc72808c6b21ec4
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-05-12 23:57:47 -0400

    Added numToMinuteSecond()

--- src/utils/time-helpers.ts
+++ src/utils/time-helpers.ts
@@ -4,9 +4,21 @@
  * @param timeString string in the format "minutes:seconds"
  * @returns a number representing the total seconds
  */
-function minuteSecondStringToNum(timeString: string): number {
+export function minuteSecondStringToNum(timeString: string): number {
     const [minutes, seconds] = timeString.split(":").map(Number);
     const totalSeconds = minutes * 60 + seconds;
 
     return totalSeconds;
+}
+
+/**
+ * Takes a number representing total seconds and returns a string in the format "minutes:seconds".
+ * @param totalSeconds number representing the total seconds
+ * @returns a string in the format "minutes:seconds"
+ */
+export function numToMinuteSecond(totalSeconds: number) {
+    const minutes = Math.floor(totalSeconds / 60); // floor to exclude the seconds
+    const seconds = totalSeconds % 60; // the remainder is the seconds
+
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`; // pad seconds with zeros for consistency
 }
