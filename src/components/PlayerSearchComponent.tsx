dcf5d9a3762e1131ac67675e8818581df094ecc0
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-05-14 12:39:40 -0400

    Trying to fix type issue

--- src/components/PlayerSearchComponent.tsx
+++ src/components/PlayerSearchComponent.tsx
@@ -2,14 +2,14 @@ import { Input, Button } from "@chakra-ui/react";
 import { useState } from "react";
 import { searchLeagueForPlayer } from "../utils/nhl-api-helpers";
 
-const PlayerSearchComponent = ({ onPlayerSearch }) => {
+const PlayerSearchComponent = ({ onPlayerSearch }: any) => {
     const [searchText, setSearchText] = useState("");
 
-    const handleInputChange = (event) => {
+    const handleInputChange = (event: any) => {
         setSearchText(event.target.value);
     };
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = async (event: any) => {
         event.preventDefault(); // Prevent form submission refresh
         const foundPlayers = await searchLeagueForPlayer(searchText);
         const firstPlayerID = foundPlayers[0]?.id; // Get the ID of the first player
