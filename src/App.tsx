469f32a23907e9430859f183b3da1eac706aa7d0
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-05-14 01:07:16 -0400

    Added test SingleStatPlayerLineChart

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import SingleStatPlayerLineChart from "./components/SingleStatPlayerLineChart";
 import "./utils/nhl-api-helpers";
 
 function App() {
     return (
         <>
             <h1>Hi =)</h1>
+            <SingleStatPlayerLineChart playerID={8476981} statType="goals" />
         </>
     );
 }
