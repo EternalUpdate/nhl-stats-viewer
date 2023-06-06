1d0068b0a8740a33edfd788a144d8a9d16259889
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-06-06 15:17:14 -0400

    Added Helmet for page title metadata

--- src/pages/PlayerSeasonGraphsPage.tsx
+++ src/pages/PlayerSeasonGraphsPage.tsx
@@ -13,12 +13,14 @@ import { Section } from "../components/Section";
 import { Subsection } from "../components/Subsection";
 import PlayerSearchComponent from "../components/PlayerSearchComponent";
 import PlayerInfoComponent from "../components/PlayerInfoComponent";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 // test
 import "../utils/nhl-api-helpers";
 import "../utils/time-helpers";
+import { getPlayerInfo } from "../utils/nhl-api-helpers";
 
 const PlayerSeasonGraphsPage = () => {
     // Get the player ID from the URL (if applicable)
@@ -34,6 +36,18 @@ const PlayerSeasonGraphsPage = () => {
 
     const [playerID, setPlayerID] = useState<number>(defaultPlayerID);
     const [playoffs, setPlayoffs] = useState<boolean>(false);
+    const [playerName, setPlayerName] = useState<string | undefined>(undefined);
+
+    useEffect(() => {
+        getPlayerInfo(playerID).then((playerInfo) => {
+            setPlayerName(playerInfo?.fullName);
+        });
+    }, [playerID]);
+
+    // page title
+    const pageTitle = playerName
+        ? `${playerName} | NHL Stats Viewer`
+        : "NHL Stats Viewer";
 
     const handlePlayerSearch = async (playerID: number) => {
         if (playerID) {
@@ -52,202 +66,213 @@ const PlayerSeasonGraphsPage = () => {
 
     return (
         <>
-            <PlayerSearchComponent onPlayerSearch={handlePlayerSearch} />
+            <HelmetProvider>
+                <Helmet>
+                    <title>{pageTitle}</title>
+                </Helmet>
+                <PlayerSearchComponent onPlayerSearch={handlePlayerSearch} />
 
-            <Heading size="lg" fontWeight="semibold" pt={{ base: "1" }}>
-                NHL Stats Viewer
-            </Heading>
-            <Text fontSize="lg" color="gray.500">
-                See progress over time{" "}
-            </Text>
+                <Heading size="lg" fontWeight="semibold" pt={{ base: "1" }}>
+                    NHL Stats Viewer
+                </Heading>
+                <Text fontSize="lg" color="gray.500">
+                    See progress over time{" "}
+                </Text>
 
-            <VStack
-                spacing={{ base: "8", md: "20" }}
-                mb={{ base: "0", md: "-8" }}
-            >
-                <PlayerInfoComponent playerID={playerID}></PlayerInfoComponent>
-                <FormControl
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
+                <VStack
+                    spacing={{ base: "8", md: "20" }}
+                    mb={{ base: "0", md: "-8" }}
                 >
-                    <Switch
-                        id="playoffs-toggle"
-                        onChange={handlePlayoffsToggle}
-                    />
-                    <FormLabel htmlFor="playoffs-toggle-label" mb="0" ml="2">
-                        Playoffs
-                    </FormLabel>
-                </FormControl>
-            </VStack>
+                    <PlayerInfoComponent
+                        playerID={playerID}
+                    ></PlayerInfoComponent>
+                    <FormControl
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Switch
+                            id="playoffs-toggle"
+                            onChange={handlePlayoffsToggle}
+                        />
+                        <FormLabel
+                            htmlFor="playoffs-toggle-label"
+                            mb="0"
+                            ml="2"
+                        >
+                            Playoffs
+                        </FormLabel>
+                    </FormControl>
+                </VStack>
 
-            <Section title="Production">
-                <PlayerStatLineChart
-                    playerID={playerID}
-                    statTypes={["goals", "assists", "points"]}
-                    title="Goals, Assists, and Points"
-                    playoffs={playoffs}
-                />
-                <Subsection>
+                <Section title="Production">
                     <PlayerStatLineChart
                         playerID={playerID}
-                        statTypes={["shots"]}
-                        title="Shots"
+                        statTypes={["goals", "assists", "points"]}
+                        title="Goals, Assists, and Points"
                         playoffs={playoffs}
                     />
-                    <PlayerStatLineChart
-                        playerID={playerID}
-                        statTypes={["shotPct"]}
-                        title="Shot Percentage"
-                        playoffs={playoffs}
-                    />
-                </Subsection>
-            </Section>
+                    <Subsection>
+                        <PlayerStatLineChart
+                            playerID={playerID}
+                            statTypes={["shots"]}
+                            title="Shots"
+                            playoffs={playoffs}
+                        />
+                        <PlayerStatLineChart
+                            playerID={playerID}
+                            statTypes={["shotPct"]}
+                            title="Shot Percentage"
+                            playoffs={playoffs}
+                        />
+                    </Subsection>
+                </Section>
 
-            <Section title="Health and Utilization">
-                <PlayerStatLineChart
-                    playerID={playerID}
-                    statTypes={["games"]}
-                    title="Games Played"
-                    playoffs={playoffs}
-                />
-                <Subsection>
+                <Section title="Health and Utilization">
                     <PlayerStatLineChart
                         playerID={playerID}
-                        statTypes={["timeOnIcePerGame"]}
-                        title="Average Time On Ice Per Game"
+                        statTypes={["games"]}
+                        title="Games Played"
                         playoffs={playoffs}
                     />
-                    <PlayerStatLineChart
-                        playerID={playerID}
-                        statTypes={["timeOnIce"]}
-                        title="Total Time on Ice per Season"
-                        playoffs={playoffs}
-                    />
-                </Subsection>
-                <Subsection>
-                    <PlayerStatLineChart
-                        playerID={playerID}
-                        statTypes={["evenTimeOnIcePerGame"]}
-                        title="Average Even Strength Time On Ice Per Game"
-                        playoffs={playoffs}
-                    />
-                    <PlayerStatLineChart
-                        playerID={playerID}
-                        statTypes={["evenTimeOnIce"]}
-                        title="Even Strength Total Time on Ice per Season"
-                        playoffs={playoffs}
-                    />
-                </Subsection>
+                    <Subsection>
+                        <PlayerStatLineChart
+                            playerID={playerID}
+                            statTypes={["timeOnIcePerGame"]}
+                            title="Average Time On Ice Per Game"
+                            playoffs={playoffs}
+                        />
+                        <PlayerStatLineChart
+                            playerID={playerID}
+                            statTypes={["timeOnIce"]}
+                            title="Total Time on Ice per Season"
+                            playoffs={playoffs}
+                        />
+                    </Subsection>
+                    <Subsection>
+                        <PlayerStatLineChart
+                            playerID={playerID}
+                            statTypes={["evenTimeOnIcePerGame"]}
+                            title="Average Even Strength Time On Ice Per Game"
+                            playoffs={playoffs}
+                        />
+                        <PlayerStatLineChart
+                            playerID={playerID}
+                            statTypes={["evenTimeOnIce"]}
+                            title="Even Strength Total Time on Ice per Season"
+                            playoffs={playoffs}
+                        />
+                    </Subsection>
 
-                <PlayerStatLineChart
-                    playerID={playerID}
-                    statTypes={["shifts"]}
-                    title="Shifts"
-                    playoffs={playoffs}
-                />
-            </Section>
-
-            <Section title="Physicality">
-                <PlayerStatLineChart
-                    playerID={playerID}
-                    statTypes={["hits"]}
-                    title="Hits"
-                    playoffs={playoffs}
-                />
-                <PlayerStatLineChart
-                    playerID={playerID}
-                    statTypes={["pim"]}
-                    title="Penalty Minutes"
-                    playoffs={playoffs}
-                />
-            </Section>
-
-            <Section title="Special Units">
-                <PlayerStatLineChart
-                    playerID={playerID}
-                    statTypes={["powerPlayGoals", "powerPlayPoints"]}
-                    title="Power Play (Production)"
-                    playoffs={playoffs}
-                />
-                <Subsection>
                     <PlayerStatLineChart
                         playerID={playerID}
-                        statTypes={["powerPlayTimeOnIcePerGame"]}
-                        title="Power Play (Average TOI per Game)"
+                        statTypes={["shifts"]}
+                        title="Shifts"
+                        playoffs={playoffs}
+                    />
+                </Section>
+
+                <Section title="Physicality">
+                    <PlayerStatLineChart
+                        playerID={playerID}
+                        statTypes={["hits"]}
+                        title="Hits"
                         playoffs={playoffs}
                     />
                     <PlayerStatLineChart
                         playerID={playerID}
-                        statTypes={["powerPlayTimeOnIce"]}
-                        title="Power Play (Total TOI per Season)"
+                        statTypes={["pim"]}
+                        title="Penalty Minutes"
                         playoffs={playoffs}
                     />
-                </Subsection>
+                </Section>
 
-                <PlayerStatLineChart
-                    playerID={playerID}
-                    statTypes={["shortHandedGoals", "shortHandedPoints"]}
-                    title="Penalty Kill"
-                    playoffs={playoffs}
-                />
-                <Subsection>
+                <Section title="Special Units">
                     <PlayerStatLineChart
                         playerID={playerID}
-                        statTypes={["shortHandedTimeOnIcePerGame"]}
-                        title="Penalty Kill (Average TOI per Game)"
+                        statTypes={["powerPlayGoals", "powerPlayPoints"]}
+                        title="Power Play (Production)"
+                        playoffs={playoffs}
+                    />
+                    <Subsection>
+                        <PlayerStatLineChart
+                            playerID={playerID}
+                            statTypes={["powerPlayTimeOnIcePerGame"]}
+                            title="Power Play (Average TOI per Game)"
+                            playoffs={playoffs}
+                        />
+                        <PlayerStatLineChart
+                            playerID={playerID}
+                            statTypes={["powerPlayTimeOnIce"]}
+                            title="Power Play (Total TOI per Season)"
+                            playoffs={playoffs}
+                        />
+                    </Subsection>
+
+                    <PlayerStatLineChart
+                        playerID={playerID}
+                        statTypes={["shortHandedGoals", "shortHandedPoints"]}
+                        title="Penalty Kill"
+                        playoffs={playoffs}
+                    />
+                    <Subsection>
+                        <PlayerStatLineChart
+                            playerID={playerID}
+                            statTypes={["shortHandedTimeOnIcePerGame"]}
+                            title="Penalty Kill (Average TOI per Game)"
+                            playoffs={playoffs}
+                        />
+                        <PlayerStatLineChart
+                            playerID={playerID}
+                            statTypes={["shortHandedTimeOnIce"]}
+                            title="Penalty Kill (Total TOI per Season)"
+                            playoffs={playoffs}
+                        />
+                    </Subsection>
+                </Section>
+
+                <Section title="Clutch Factor">
+                    <PlayerStatLineChart
+                        playerID={playerID}
+                        statTypes={["gameWinningGoals", "overTimeGoals"]}
+                        title="Game Winning and Overtime Goals"
+                        playoffs={playoffs}
+                    />
+                </Section>
+
+                <Section title="Defense">
+                    <PlayerStatLineChart
+                        playerID={playerID}
+                        statTypes={["blocked"]}
+                        title="Blocked Shots"
                         playoffs={playoffs}
                     />
                     <PlayerStatLineChart
                         playerID={playerID}
-                        statTypes={["shortHandedTimeOnIce"]}
-                        title="Penalty Kill (Total TOI per Season)"
+                        statTypes={["plusMinus"]}
+                        title="Plus Minus"
                         playoffs={playoffs}
                     />
-                </Subsection>
-            </Section>
+                </Section>
 
-            <Section title="Clutch Factor">
-                <PlayerStatLineChart
-                    playerID={playerID}
-                    statTypes={["gameWinningGoals", "overTimeGoals"]}
-                    title="Game Winning and Overtime Goals"
-                    playoffs={playoffs}
-                />
-            </Section>
-
-            <Section title="Defense">
-                <PlayerStatLineChart
-                    playerID={playerID}
-                    statTypes={["blocked"]}
-                    title="Blocked Shots"
-                    playoffs={playoffs}
-                />
-                <PlayerStatLineChart
-                    playerID={playerID}
-                    statTypes={["plusMinus"]}
-                    title="Plus Minus"
-                    playoffs={playoffs}
-                />
-            </Section>
-
-            <Section title="Face-offs">
-                <PlayerStatLineChart
-                    playerID={playerID}
-                    statTypes={["faceOffPct"]}
-                    title="Face-Off Percentage"
-                    playoffs={playoffs}
-                />
-            </Section>
-            <Text fontSize="sm" mt="28" color="gray.500">
-                Brought to you by{" "}
-                <Link
-                    href="https://github.com/EternalUpdate/nhl-stats-viewer/tree/main"
-                    target="_blank"
-                >
-                    EternalUpdate
-                </Link>
-            </Text>
+                <Section title="Face-offs">
+                    <PlayerStatLineChart
+                        playerID={playerID}
+                        statTypes={["faceOffPct"]}
+                        title="Face-Off Percentage"
+                        playoffs={playoffs}
+                    />
+                </Section>
+                <Text fontSize="sm" mt="28" color="gray.500">
+                    Brought to you by{" "}
+                    <Link
+                        href="https://github.com/EternalUpdate/nhl-stats-viewer/tree/main"
+                        target="_blank"
+                    >
+                        EternalUpdate
+                    </Link>
+                </Text>
+            </HelmetProvider>
         </>
     );
 };
