a14e3181394ed3cf2f4bbf6d9b5890a4041e2189
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-06-07 00:58:36 -0400

    Added route to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import PlayerSeasonGraphsPage from "./pages/PlayerSeasonGraphsPage";
 import { Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import Home from "./pages/Home";
 
 function App() {
     return (
         <>
-            <Routes>
-                <Route path="/" element={<PlayerSeasonGraphsPage />} />
-                <Route
-                    path="/player/:playerID"
-                    element={<PlayerSeasonGraphsPage />}
-                />
-            </Routes>
+            <HelmetProvider>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route
+                        path="/player/:playerID"
+                        element={<PlayerSeasonGraphsPage />}
+                    />
+                </Routes>
+            </HelmetProvider>
         </>
     );
 }
