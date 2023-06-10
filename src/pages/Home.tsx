761ce1fbff3c7cdbd6a958965d9b4b65b999c117
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-06-10 15:45:45 -0400

    Fixed the wave and converted Home.css to a CSS module

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import PlayerSearchComponent from "../components/PlayerSearchComponent";
 import { useNavigate } from "react-router-dom";
-import { Heading, Text, Flex, Box } from "@chakra-ui/react";
-import "./Home.css";
+import { Heading, Text, Flex, Box, Link } from "@chakra-ui/react";
+import styles from "./Home.module.css";
 
 const Home = () => {
     const navigate = useNavigate();
@@ -19,23 +19,35 @@ const Home = () => {
                 width={"100%"}
                 paddingY={"2em"}
             >
-                <Flex className="home-header-container">
-                    <Heading
-                        className="home-title"
-                        fontSize={"1.2rem"}
-                        fontWeight={600}
-                        color={"gray.700"}
+                <Flex className={styles.homeHeaderContainer}>
+                    <Flex
+                        width={"100%"}
+                        justifyContent={"space-between"}
+                        alignItems={"baseline"}
                     >
-                        NHL Stats Viewer
-                    </Heading>
-                    <Text className="home-subtitle">
+                        <Text className={styles.homeTitle}>
+                            NHL Stats Viewer
+                        </Text>
+                        <Text className={styles.credits}>
+                            Brought to you by{" "}
+                            <Link
+                                href="https://github.com/EternalUpdate/nhl-stats-viewer/tree/main"
+                                target="_blank"
+                            >
+                                EternalUpdate
+                            </Link>
+                        </Text>
+                    </Flex>
+                    <Text className={styles.homeSubtitle}>
                         See progress over time{" "}
                     </Text>
                 </Flex>
-                <Box className="home-hero-container">
-                    <Heading className="home-hero-title">
+                <Box className={styles.homeHeroContainer}>
+                    <Heading className={styles.homeHeroTitle}>
                         Look up current NHL player stats{" "}
-                        <Text className="home-hero-emphasis">visually.</Text>
+                        <Text className={styles.homeHeroEmphasis}>
+                            visually.
+                        </Text>
                     </Heading>
                     <Box>
                         <PlayerSearchComponent
@@ -43,19 +55,21 @@ const Home = () => {
                         />
                     </Box>
                 </Box>
-                <Box>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 1440 320"
-                        className="home-wave-svg"
-                    >
-                        <path
-                            fill="#f3f4f5"
-                            fill-opacity="1"
-                            d="M0,192L40,181.3C80,171,160,149,240,144C320,139,400,149,480,176C560,203,640,245,720,229.3C800,213,880,139,960,117.3C1040,96,1120,128,1200,128C1280,128,1360,96,1400,80L1440,64L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
-                        ></path>
-                    </svg>
-                    <Box className="home-wave-continuation-fill"></Box>
+                <Box className={styles.waveSvgBox}>
+                    <div className={styles.waveDivider}>
+                        <svg
+                            data-name="Layer 1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 1200 120"
+                            preserveAspectRatio="none"
+                        >
+                            <path
+                                d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+                                className={styles.shapeFill}
+                            ></path>
+                        </svg>
+                        <Box className={styles.waveSvgBottomFill}></Box>
+                    </div>
                 </Box>
             </Flex>
         </>
